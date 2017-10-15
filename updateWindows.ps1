2de54b5c4525a5c6cc2079673c53cc32388d7b39
Joaquín Fernández Campo <jfcampo@gmail.com>
2017-10-15 17:06:11 +0200

    Added laplock!

--- updateWindows.ps1
+++ updateWindows.ps1
@@ -1,2 +1,6 @@
 Set-Variable -Name uriGist -Value "https://gist.githubusercontent.com/xocasdashdash/2bb1d74adf954a21f343e3c681bf1aad/raw"
+Get-ChildItem "modules\*.ps1" | %{.$_} 
+$currPath=(Get-Item -Path ".\" -Verbose).FullName
+Add-StartMenuShortcut -Name "laplock" -Folder "Start-up" -Target "${currPath}\laplock\Debug\laplock.exe" -WorkingDirectory "${currPath}\laplock\Debug" -Icon "${currPath}\laplock\Debug\laplock.exe" -IconIndex 0 -Description "Lock Laptop" -Force
+
 Install-BoxstarterPackage -PackageName $uriGist -DisableReboots
