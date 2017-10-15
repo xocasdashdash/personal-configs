0faa2dbc8f74bec6dcc722412fff629c93dcaefb
Joaquín Fernández Campo <jfcampo@gmail.com>
2017-10-15 18:34:23 +0200

    Enabled 10 installs

--- updateWindows.ps1
+++ updateWindows.ps1
@@ -1,6 +1,7 @@
 Set-Variable -Name uriGist -Value "https://gist.githubusercontent.com/xocasdashdash/2bb1d74adf954a21f343e3c681bf1aad/raw"
 Get-ChildItem "modules\*.ps1" | %{.$_} 
 $currPath=(Get-Item -Path ".\" -Verbose).FullName
+Write-Output "Path: ${currPath}"
 Add-StartMenuShortcut -Name "laplock" -Folder "Start-up" -Target "${currPath}\laplock\Debug\laplock.exe" -WorkingDirectory "${currPath}\laplock\Debug" -Icon "${currPath}\laplock\Debug\laplock.exe" -IconIndex 0 -Description "Lock Laptop" -Force
-
+Write-Output "Using gist: ${uriGist}"
 Install-BoxstarterPackage -PackageName $uriGist -DisableReboots
